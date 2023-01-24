091e269760c669a66651c8929729fe396e715ace
sunil-kumar-sahu <100436257+sunil-kumar-sahu@users.noreply.github.com>
2023-01-24 12:49:16 +0530

    Add files via upload

---
+++ BootDataJpaProj10-CallingPL-SQLPRocedure-Oracle/src/main/java/com/nt/repository/IMovieRepo.java
@@ -0,0 +1,16 @@
+package com.nt.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.nt.model.Movie;
+
+@Transactional
+public interface IMovieRepo extends JpaRepository<Movie, Integer> {
+	
+		
+
+}
