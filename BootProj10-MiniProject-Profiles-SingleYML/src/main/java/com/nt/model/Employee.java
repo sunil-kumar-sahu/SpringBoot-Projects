1365a64cd8ba031533ab218a2c603719c82f002c
sunil-kumar-sahu <100436257+sunil-kumar-sahu@users.noreply.github.com>
2023-01-24 13:05:30 +0530

    Add files via upload

---
+++ BootProj10-MiniProject-Profiles-SingleYML/src/main/java/com/nt/model/Employee.java
@@ -0,0 +1,34 @@
+package com.nt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+	private   Integer eno;
+	private  String ename;
+	private  String ejob;
+	private Float  esal;
+	public void setEno(int int1) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setEname(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setJob(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setSal(float float1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
