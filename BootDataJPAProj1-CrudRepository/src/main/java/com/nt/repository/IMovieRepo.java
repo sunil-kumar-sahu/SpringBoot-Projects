6855f122ac1b5f8fa33a91e240cde2a34c3250d4
sunil-kumar-sahu <100436257+sunil-kumar-sahu@users.noreply.github.com>
2023-01-24 11:19:46 +0530

    Add files via upload

---
+++ BootDataJPAProj1-CrudRepository/src/main/java/com/nt/repository/IMovieRepo.java
@@ -0,0 +1,10 @@
+package com.nt.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.nt.entity.Movie;
+
+
+public interface IMovieRepo extends CrudRepository<Movie,Integer>{
+
+}
